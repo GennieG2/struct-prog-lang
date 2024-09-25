32d9a4ec3b573d9475d8a2853d4471199cabf06a
GennieG2 <159817565+GennieG2@users.noreply.github.com>
2024-09-25 17:51:46 +0000

    fixed some changes

--- gennie-topic-01-simple-expressions/evaluator.py
+++ gennie-topic-01-simple-expressions/evaluator.py
@@ -29,6 +29,10 @@ def evaluate(ast, environment):
         left_value, _ = evaluate(ast["left"], environment)
         right_value, _ = evaluate(ast["right"], environment)
         return left_value == right_value, False
+    if ast["tag"] == "<":
+        left_value, _ = evaluate(ast["left"], environment)
+        right_value, _ = evaluate(ast["right"], environment)
+        return left_value < right_value, False 
     assert False, "Unknown operator in AST"
 
 def equals(code, environment, expected_result, expected_environment=None):
@@ -89,6 +93,12 @@ def test_evaluate_isEqual():
     equals("2==3", {}, False, {})
     equals("4*(2+3)==20", {}, True, {})
 
+def test_evaluate_lessThan():
+    print("test < operator")
+    equals("2<3", {}, True, {})
+    equals("2<1", {}, False, {})
+    equals("4*(2+3)<22", {}, True, {})
+
 if __name__ == "__main__":
     test_evaluate_single_value()
     test_evaluate_addition()
@@ -97,4 +107,5 @@ if __name__ == "__main__":
     test_evaluate_division()
     test_evaluate_negation()
     test_evaluate_isEqual()
+    test_evaluate_lessThan()
     print("done.")
